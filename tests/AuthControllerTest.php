43537e8ff9a791c777d59788608edd923b5d2b2a
Jace Ju <jaceju@gmail.com>
2015-03-06 01:21:33 +0800

    Refactor code

--- tests/AuthControllerTest.php
+++ tests/AuthControllerTest.php
@@ -23,32 +23,25 @@ class AuthControllerTest extends TestCase
 
     public function testLoginSuccess()
     {
-        // Mock Auth Guard Object
-        $guardMock = Mockery::mock('Illuminate\Auth\Guard');
-        $this->app->instance('Illuminate\Contracts\Auth\Guard', $guardMock);
-
-        /* @see App\Http\Middleware\RedirectIfAuthenticated */
-        $guardMock
-            ->shouldReceive('check')
-            ->once()
-            ->andReturn(false);
-
-        /* @see Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers */
-        $guardMock
-            ->shouldReceive('attempt')
-            ->once()
-            ->andReturn(true);
-
-        $this->call('POST', 'auth/login', [
-            'email'    => 'jaceju@gmail.com',
-            'password' => 'password',
-            '_token'   => csrf_token(),
-        ]);
-
-        $this->assertRedirectedTo('home');
+        $this->doesLoginPass(true);
     }
 
     public function testLoginFailed()
+    {
+        $this->doesLoginPass(false);
+    }
+
+    public function testLogout()
+    {
+
+    }
+
+    public function testRegister()
+    {
+
+    }
+
+    protected function doesLoginPass($pass)
     {
         // Mock Auth Guard Object
         $guardMock = Mockery::mock('Illuminate\Auth\Guard');
@@ -64,7 +57,7 @@ class AuthControllerTest extends TestCase
         $guardMock
             ->shouldReceive('attempt')
             ->once()
-            ->andReturn(false);
+            ->andReturn($pass);
 
         $this->call('POST', 'auth/login', [
             'email'    => 'jaceju@gmail.com',
@@ -72,8 +65,12 @@ class AuthControllerTest extends TestCase
             '_token'   => csrf_token(),
         ]);
 
-        $this->assertHasOldInput();
-        $this->assertSessionHasErrors();
-        $this->assertRedirectedTo('auth/login');
+        if ($pass) {
+            $this->assertRedirectedTo('home');
+        } else {
+            $this->assertHasOldInput();
+            $this->assertSessionHasErrors();
+            $this->assertRedirectedTo('auth/login');
+        }
     }
 }
